73a93afc0ad8a28282d7ba4405e70e6bceecf5d3
madsid <sidhartha.m@vemoworkforce.com>
2016-10-03 16:25:37 -0500

    Maruthi code merge

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -2,3 +2,15 @@
 $('document').ready(function(){
     //new Vidage('#VidageVideo');
 });
+$(window).scroll(function(){
+    if($(document).scrollTop() > ($(window).height())*.88) {
+      $('.logo').css({'height': '8vh'});
+      $('ul.nav li a').css({'padding-top' : 'calc(4vh - 10px)', 'padding-bottom' : 'calc(4vh - 10px)'});
+      $('.custom-ham').css({'margin-top' : 'calc(4vh - 10px)', 'margin-bottom' : 'calc(4vh - 10px)'});
+    }
+    else {
+      $('.logo').css({'height': '15vh'});
+      $('ul.nav li a').css({'padding-top' : 'calc(7.5vh - 10px)', 'padding-bottom' : 'calc(7.5vh - 10px)'});
+      $('.custom-ham').css({'margin-top' : 'calc(7.5vh - 10px)', 'margin-bottom' : 'calc(7.5vh - 10px)'});
+    }
+});
